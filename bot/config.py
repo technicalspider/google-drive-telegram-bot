fc27c73d4663ea08f328b3c6f530d5d491837c91
Technical Spider <69822899+technicalspider@users.noreply.github.com>
2020-12-08 15:21:32 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,11 +1,10 @@
 class config:
-    BOT_TOKEN = ""
-    APP_ID = ""
-    API_HASH = ""
-    DATABASE_URL = ""
-    SUDO_USERS = "" # Sepearted by space.
+    BOT_TOKEN = "1426725008:AAEI16H16lLYH2rSdTUEMghiUcrCn27V8aE"
+    APP_ID = "1380843"
+    API_HASH = "1aa2218bfe4b27c799e2d856f8a03b54"
+    DATABASE_URL = "postgres://feykhbbuxzhjvq:00eb710a27764ac408aaae05294f45df34384382a07d0bdc335efe51de8e1c76@ec2-54-86-170-8.compute-1.amazonaws.com:5432/d3rlgn02q9eu66"
+    SUDO_USERS = "778114097" # Sepearted by space.
     SUPPORT_CHAT_LINK = ""
-    DOWNLOAD_DIRECTORY = "./downloads/"
 
 
 class BotCommands:
